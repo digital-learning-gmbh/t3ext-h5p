d82e1bcd343b432277f715c9058d673d63f2e348
Benjamin Ledel <benjamin.ledel@digitallearning.gmbh>
2025-05-18 21:25:26 +0200

    Update LibraryDependencyRepository.php

--- Classes/Domain/Repository/LibraryDependencyRepository.php
+++ Classes/Domain/Repository/LibraryDependencyRepository.php
@@ -1,43 +1,22 @@
 <?php
 namespace MichielRoos\H5p\Domain\Repository;
 
-/*
- * This file is part of the TYPO3 CMS project.
- *
- * It is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License, either version 2
- * of the License, or any later version.
- *
- * For the full copyright and license information, please read the
- * LICENSE.txt file that was distributed with this source code.
- *
- * The TYPO3 project - inspiring people to share!
- */
-use TYPO3\CMS\Extbase\Persistence\Generic\QuerySettingsInterface;
+use TYPO3\CMS\Extbase\Persistence\Generic\Typo3QuerySettings;
 use TYPO3\CMS\Extbase\Persistence\Repository;
 
-/**
- * Class LibraryDependencyRepository
- */
 class LibraryDependencyRepository extends Repository
 {
-    /**
-     * initializes any required object
-     */
-    public function initializeObject()
+    public function __construct(Typo3QuerySettings $querySettings = null)
     {
-        if ($this->defaultQuerySettings === null) {
-            $this->defaultQuerySettings = $this->objectManager->get(QuerySettingsInterface::class);
+        parent::__construct();
+
+        if ($querySettings !== null) {
+            $querySettings->setRespectStoragePage(false);
+            $this->setDefaultQuerySettings($querySettings);
         }
-        $this->defaultQuerySettings->setRespectStoragePage(false);
     }
 
-    /**
-     * @param string $library
-     * @param string $requiredLibrary
-     * @return object|null
-     */
-    public function findOneByLibraryAndRequiredLibrary($library, $requiredLibrary)
+    public function findOneByLibraryAndRequiredLibrary(string $library, string $requiredLibrary)
     {
         $query = $this->createQuery();
         $dependencies = $query->matching(
@@ -46,9 +25,7 @@ class LibraryDependencyRepository extends Repository
                 $query->equals('required_library', $requiredLibrary)
             )
         )->execute();
-        if ($dependencies->count()) {
-            return $dependencies->getFirst();
-        }
-        return null;
+
+        return $dependencies->count() ? $dependencies->getFirst() : null;
     }
 }
