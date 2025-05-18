cf9958936106761e4a6b0397c4760ff3f1ebf9b8
Benjamin Ledel <benjamin.ledel@digitallearning.gmbh>
2025-05-18 16:18:34 +0200

    Update H5pModuleController.php

--- Classes/Controller/H5pModuleController.php
+++ Classes/Controller/H5pModuleController.php
@@ -38,7 +38,6 @@ use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Core\Utility\PathUtility;
 use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
-use TYPO3\CMS\Backend\View\BackendTemplateView;
 use TYPO3\CMS\Extbase\Mvc\Web\Routing\UriBuilder;
 use TYPO3\CMS\Extbase\Property\PropertyMappingConfiguration;
 
